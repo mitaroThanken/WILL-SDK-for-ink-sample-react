d718f14feb49f4927ee45a8776c50f28f093f63f
YAMAMI Taro <mitarothanken@gmail.com>
2022-07-17 21:34:31 +0000

    Color picker

--- sample/src/components/ColorPicker.tsx
+++ sample/src/components/ColorPicker.tsx
@@ -1,9 +1,36 @@
-import { Avatar, Button } from "@mui/material";
+import { Button, Paper, styled } from "@mui/material";
+import { useRef, useState } from "react";
 
-export const ColorPicker = () => {
+interface ColorPickerProps {
+  onChange?: (color: string) => void
+}
+
+export const ColorPicker = ({ onChange }: ColorPickerProps) => {
+  const [color, setColor] = useState("#4A4A4A");
+  const refInput = useRef<HTMLInputElement>(null);
+
+  const StyledPaper = styled(Paper)(() => ({
+    backgroundColor: color
+  }));
+
   return (
     <Button>
-      <Avatar variant="rounded" alt="Color"> </Avatar>
+      <StyledPaper sx={{ width: "44px", height: "44px" }} elevation={0} />
+      <input type="color" defaultValue={color} ref={refInput} style={{
+        display: "block",
+        position: "absolute",
+        left: 0,
+        top: 0,
+        width: "100%",
+        height: "100%",
+        opacity: 0,
+        cursor: "pointer"
+      }}
+        onChange={(e) => {
+          setColor(e.currentTarget.value);
+          onChange && onChange(e.currentTarget.value);
+        }}
+      />
     </Button>
   )
 }
